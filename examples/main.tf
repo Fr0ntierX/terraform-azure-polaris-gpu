6be3046261f066422e08e2e264451ad53a724797
Karol Legut <karol.legut121@gmail.com>
2025-03-17 13:30:52 +0100

    feat: add example Terraform configuration for Polaris Azure GPU module

---
+++ examples/main.tf
@@ -0,0 +1,25 @@
+module "polaris_azure_gpu_module" {
+  source = "../"
+
+  subscription_id = "3bc57494-c0a6-4ee2-b44d-8b398b4a8162"
+
+  name     = "polaris-gpu-example"
+  location = "eastus2"
+  zone     = "2"
+
+  admin_username        = "azureuser"
+  authentication_type   = "password"
+  admin_password_or_key = "YOUR_PASSWORD"
+
+  workload_type = "customWorkload"
+
+  custom_workload_image_address = "YOUR_ACR_SERVER/your-llm-image:latest"
+  custom_workload_port          = 11434
+
+  custom_workload_image_registry_login_server = "YOUR_ACR_SERVER"
+  custom_workload_image_registry_username     = "YOUR_ACR_USERNAME"
+  custom_workload_image_registry_password     = "YOUR_ACR_PASSWORD"
+
+  polaris_proxy_port          = 3000
+  polaris_proxy_image_version = "latest"
+}
